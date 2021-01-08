8267c442470f843f4b1e0e05bc73319740e2f202
Dmitry Ovchinnikov <d_ovchinnikov@wargaming.net>
2021-01-08 12:40:15 +0100

    Types

--- marid-common/src/main/kotlin/org/marid/types/Types.kt
+++ marid-common/src/main/kotlin/org/marid/types/Types.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package org.marid.model.types
+package org.marid.types
 
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
