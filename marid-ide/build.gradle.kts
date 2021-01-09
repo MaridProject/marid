0542e42014924758a30e463ac9790c4e621d7846
Dmitry Ovchinnikov <d_ovchinnikov@wargaming.net>
2021-01-09 08:43:31 +0100

    ide

---
+++ marid-ide/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  application
+  kotlin("jvm")
+}
+
+val openjfxVersion = "15.0.1"
+
+dependencies {
+  implementation(group = "org.openjfx", name = "javafx-controls", version = openjfxVersion)
+  implementation(group = "org.openjfx", name = "javafx-media", version = openjfxVersion)
+  implementation(group = "org.openjfx", name = "javafx-swing", version = openjfxVersion)
+
+  val curOsName = System.getProperty("os.name").toLowerCase()
+  val curOs = when {
+    curOsName.contains("win") -> "win"
+    curOsName.contains("mac") -> "mac"
+    else -> "linux"
+  }
+
+  implementation(group = "org.openjfx", name = "javafx-controls", version = openjfxVersion, classifier = curOs)
+  implementation(group = "org.openjfx", name = "javafx-media", version = openjfxVersion, classifier = curOs)
+  implementation(group = "org.openjfx", name = "javafx-base", version = openjfxVersion, classifier = curOs)
+  implementation(group = "org.openjfx", name = "javafx-swing", version = openjfxVersion, classifier = curOs)
+  implementation(group = "org.openjfx", name = "javafx-graphics", version = openjfxVersion, classifier = curOs)
+}
+
